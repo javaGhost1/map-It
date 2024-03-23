4ab92ecf02ac76fef58e4d5cfcaeb27705039e1a
JavaGhost1 <Secret09!>
2024-03-23 11:52:23 +0300

    Improved the code logic ensuring accurate results

--- mapIt.py
+++ mapIt.py
@@ -7,14 +7,28 @@ def mapIt():
     if len(sys.argv) > 1:
         # get the address
         address = ' '.join(sys.argv[1:])
+        # open the browser page and map the address
+        webbrowser.open('https://www.google.com/maps/place/' + address)
 
     else:
-        # get address from clipboard
-        address = pyperclip.paste()
+        # retrieve text from clipboard
+        clipboard_content = pyperclip.paste().strip() #remove leading and trailing white spaces
+        print(clipboard_content)
+        if clipboard_content:
+            # split address by new line
+            addresses = clipboard_content.split('\n')
+            for address in addresses:
+                webbrowser.open('https://www.google.com/maps/place/' + address.strip())
 
-    webbrowser.open('https://www.google.com/maps/place/' + address)
+        else:
+            print("No address provided")
+
+
+    
 
 if __name__ == "__main__":
+    # runs this function when script is run directly
+
     mapIt()
 
     # sudo ln -s /Desktop/development/webscraping/mapIt/mapIt.py /usr/local/bin/mapit
